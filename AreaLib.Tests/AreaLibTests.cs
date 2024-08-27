f3b854017dfd7d929b8128366162f56aac3eec19
Winner <Winner@DESKTOP-G97RQ8V>
2024-08-27 18:10:20 +0400

    Add project files.

---
+++ AreaLib.Tests/AreaLibTests.cs
@@ -0,0 +1,58 @@
+using AreaLib;
+
+namespace AreaLib.Tests
+{
+    public class AreaLibTests
+    {
+        /// <summary>
+        /// Площадь треугольника
+        /// </summary>
+        [Fact]
+        public void GetAreaTriangleTest()
+        {
+            // Arrange
+            double a = 13;
+            double b = 14;
+            double c = 15;
+
+            // Act
+            double result = Area.GetArea(Area.Shape.Triangle, a, b, c);
+
+            // Assert
+            Assert.Equal(84, result);
+        }
+
+        /// <summary>
+        /// Площадь прямоугольного треугольника
+        /// </summary>
+        [Fact]
+        public void GetAreaRightTriangleTest()
+        {
+            // Arrange
+            double a = 6;
+            double b = 10;
+            double c = 8;
+
+            // Act
+            double result = Area.GetArea(Area.Shape.Triangle, a, b, c);
+
+            // Assert
+            Assert.Equal(24, result);
+        }
+        /// <summary>
+        /// Площадь круга
+        /// </summary>
+        [Fact]
+        public void GetAreaCircleTest()
+        {
+            // Arrange
+            double r = 5;
+
+            // Act
+            double result = Area.GetArea(Area.Shape.Circle, r: r);
+
+            // Assert
+            Assert.Equal(78.5d, result);
+        }
+    }
+}
---
+++ AreaLib/Area.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaLib
+{
+    public class Area
+    {
+        /// <summary>
+        /// Тип фигуры
+        /// </summary>
+        public enum Shape
+        {
+            Triangle,
+            Circle
+        }
+        /// <summary>
+        /// Расчёт площади фигуры
+        /// </summary>
+        /// <param name="shape">Тип фигуры</param>
+        /// <param name="a">Сторона 1</param>
+        /// <param name="b">Сторона 2</param>
+        /// <param name="c">Сторона 3</param>
+        /// <param name="r">Радиус</param>
+        /// <returns>Площадь фигуры</returns>
+        /// <exception cref="NotImplementedException"></exception>
+        public static double GetArea (Shape shape, double a = 0, double b = 0, double c = 0, double r = 0)
+        {
+            switch (shape)
+            {
+                case Shape.Triangle:
+                    Sort();
+                    if (Math.Pow(c, 2) == Math.Pow(a, 2) + Math.Pow(b, 2))
+                    {
+                        return (a*b)/2;
+                    }
+                    else {
+                        double p = (a + b + c) / 2;
+                        return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+                    }
+                case Shape.Circle:
+                    return Math.Pow(r, 2) * 3.14d;
+                default:
+                    throw new NotImplementedException();
+            }
+            void Sort()
+            {
+                double[] mas = {a, b, c};
+                Array.Sort(mas);
+                a = mas[0]; b = mas[1]; c = mas[2];
+            }
+        }
+    }
+}
